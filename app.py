4d0e315cf6976013499aed8a75ad25318ae2ba42
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-09-20 16:56:58 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def statrt():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Удодова Анастасия Александровна. Лабораторная работа 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>WEB-сервер на flask</h1>
+
+        <footer>
+            &copy; Удодова Анастасия, ФБИ-14,3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
