93e492a838b979f75bae32f2d208b9a7e1776fd2
nudodova <nastyaudo11@gmail.com>
2023-10-11 19:41:34 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
@@ -6,3 +6,9 @@ def labrab():
 
     return render_template('lab3.html')
 
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('form1.html', user=user, age=age, sex=sex)
