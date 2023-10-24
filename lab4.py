89212de27d77a866b18f05cabd0518071592d910
nudodova <nastyaudo11@gmail.com>
2023-10-24 17:50:47 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -6,3 +6,16 @@ lab4 = Blueprint('lab4', __name__)
 def lab():
     return render_template('lab4.html')
 
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'alex' and password == '123':
+        return render_template('success.html', username=username)
+
+    error = 'Неверный логин и/или пароль'
+    return render_template('login.html', error=error, username=username, password=password)
+    
