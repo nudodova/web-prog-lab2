702b96760f2ea16d3d53a73e64fc81640e8bdd2b
nudodova <nastyaudo11@gmail.com>
2023-10-27 18:23:57 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 lab4 = Blueprint('lab4', __name__)
 
 
@@ -19,3 +19,23 @@ def login():
     error = 'Неверный логин и/или пароль'
     return render_template('login.html', error=error, username=username, password=password)
     
+@lab4.route('/lab4/holodilnik', methods = ['GET', 'POST'])
+def holodilnik():
+    if request.method == 'GET':
+        return render_template('holodilnik.html')
+
+    temperature = request.form.get('temperature')
+    if not temperature:
+        vtemp  = 'Ошибка: не задана температура!'
+    elif int(temperature) < -12:
+        vtemp = 'Не удалось установить температуру - слишком низкое значение!'
+    elif int(temperature) > -1:
+        vtemp = 'Не удалось установить температуру - слишком высокое значение!'
+    elif int(temperature) > -12 and int(temperature) < -9:
+        vtemp = f"Установлена температура: {temperature}°C ***"
+    elif int(temperature) >= -8 and int(temperature) <= -5:
+        vtemp = f"Установлена температура: {temperature}°C **"
+    elif int(temperature) >= -4 and int(temperature) <= -1:
+        vtemp = f"Установлена температура: {temperature}°C *"
+    
+    return render_template('holodilnik.html', vtemp=vtemp)
