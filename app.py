dcbcbdbcbaba9f0bb8e26659e0ca109561e953f8
nudodova <nastyaudo11@gmail.com>
2023-11-28 22:58:24 +0700

    страница регистрации

--- app.py
+++ app.py
@@ -4,11 +4,27 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+
+from flask_sqlalchemy import SQLAlchemy
 from lab6 import lab6
+from Db import db
+from Db.models import users
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.secret_key = "123"
+user_db = "udodova_anastasia_knowledge_base"
+host_ip = "127.0.0.1"
+host_port = "5432"
+database_name = "knowledge_base_for_udodova_anastasia"
+password = "0123456789"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
--- lab6.py
+++ lab6.py
@@ -1,7 +1,60 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from Db import db
+from Db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, current_user
+
 lab6 = Blueprint('lab6', __name__)
 
 
 @lab6.route('/lab6/')
 def lab():
     return render_template('lab6.html')
+
+
+@lab6.route('/lab6/check')
+def main():
+    my_users = users.query.all()
+    print(my_users)
+    return "result in console!"
+
+@lab6.route('/lab6/checkarticles')
+def main2():
+    my_articles = articles.query.all()
+    print(my_articles)
+    return "result in console!"
+
+@lab6.route("/lab6/register", methods=["GET", "POST"])
+def register():
+    errors=[]
+
+    if request.method == "GET":
+        return render_template("register.html")
+
+    username_form = request.form.get("username")
+    password_form = request.form.get("password")
+
+    if not username_form:
+        errors.append("Пожалуйста, заполните поле имени!")
+        print(errors)
+        return render_template("register.html", errors=errors)
+
+    if len(password_form) < 5:
+        errors.append("Пароль должен быть не менее 5 символов!")
+        print(errors)
+        return render_template("register.html", errors=errors)
+
+    isUserExist = users.query.filter_by(username=username_form).first()
+
+    if isUserExist is not None:
+        errors.append("Пользователь с таким именем уже существует")
+        print(errors)
+        return render_template("register.html", errors=errors)
+
+    hashedPswd = generate_password_hash(password_form, method='pbkdf2')
+    newUser = users(username=username_form, password=hashedPswd)
+
+    db.session.add(newUser)
+    db.session.commit()
+
+    return redirect("/lab6/login")
