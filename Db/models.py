c081eb714806c390cf0fa0084014d995b7eff52f
nudodova <nastyaudo11@gmail.com>
2023-11-28 22:57:28 +0700

    правки

---
+++ Db/models.py
@@ -0,0 +1,23 @@
+from . import db
+
+
+class users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(102), nullable=False)
+
+    def __repr__(self):
+        return f'id:{self.id}, username:{self.username}'
+
+class articles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(50), nullable=False)
+    article_text = db.Column(db.Text, nullable=False)
+    is_favorite = db.Column(db.Boolean)
+    is_public = db.Column(db.Boolean)
+    likes = db.Column(db.Integer)
+
+    def __rerp__(self):
+        return f'title:{self.title}, article_text:{self.article_text}'
+
