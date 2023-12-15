354b761ef06f5e5841ba97f7068d5217e2fa0085
nudodova <nastyaudo11@gmail.com>
2023-12-15 12:11:00 +0700

    Получение информации о курсе.-ах по REST

--- lab8.py
+++ lab8.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template, request, abort
+from flask import Blueprint, render_template, request, abort, jsonify
 
 lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
 def main():
     return render_template('lab8/index.html')
+
+courses = [
+    {"name": "c++", "videos": 3, "price": 3000},
+    {"name": "basic", "videos": 30, "price": 0},
+    {"name": "c#", "videos": 8} 
+]
+
+@lab8.route('/lab8/api/courses/', methods=['GET'])
+def get_courses():
+    return jsonify(courses)
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    if course_num < 0 or course_num > len(courses):
+        return "Курс не найден", 404
+    return courses[course_num]
